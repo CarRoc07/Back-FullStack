e8e5d69ef8fb642b4990f591a8e8cd4d4248a024
CarRoc07 <carlovhd@gmail.com>
2023-08-01 20:51:12 -0300

    2

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,5 @@ config();
 export const ACCESS_KEY = process.env.ACCESS_TOKEN_KEY as string;
 
 export const REFRESH_KEY = process.env.REFRESH_TOKEN_KEY as string;
+
+export const PORT = process.env.PORT as string;
--- src/server/index.ts
+++ src/server/index.ts
@@ -4,13 +4,11 @@ import authRouter from '../routes/auth.router';
 import productsRouter from '../routes/products.route';
 import ordersRouter from '../routes/orders.router';
 import categoryRouter from '../routes/category.router';
+import { PORT } from '../config';
 
 const app =  express();
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
+app.use(cors())
 app.use(express.json());
 
 app.use('/api', authRouter)
@@ -18,6 +16,6 @@ app.use('/api', productsRouter)
 app.use('/api', ordersRouter)
 app.use('/api', categoryRouter)
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server is running');
 })
