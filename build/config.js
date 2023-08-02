fd8e9937683444b9470d9f6ed92c8dcdcc6b4af5
CarRoc07 <carlovhd@gmail.com>
2023-08-02 13:30:38 -0300

    7

--- build/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PORT = exports.REFRESH_KEY = exports.ACCESS_KEY = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-exports.ACCESS_KEY = process.env.ACCESS_TOKEN_KEY;
-exports.REFRESH_KEY = process.env.REFRESH_TOKEN_KEY;
-exports.PORT = process.env.PORT;
--- build/routes/products.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const productsController_1 = require("../controllers/productsController");
-const productsRouter = (0, express_1.Router)();
-productsRouter.get("/products", productsController_1.getAllProducts);
-productsRouter.post("/products", productsController_1.createProduct);
-productsRouter.delete("/products/:id", productsController_1.deleteProduct);
-exports.default = productsRouter;
