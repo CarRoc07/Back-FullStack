948d3a80a7179ec5a72f9dc6dedc26c7e5243231
CarRoc07 <carlovhd@gmail.com>
2023-08-02 13:26:26 -0300

    6

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,4 @@ export const ACCESS_KEY = process.env.ACCESS_TOKEN_KEY as string;
 
 export const REFRESH_KEY = process.env.REFRESH_TOKEN_KEY as string;
 
-export const PORT = process.env.PORT as string;
+export const PORT = process.env.PORT;
