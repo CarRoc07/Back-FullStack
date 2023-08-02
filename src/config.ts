5603760e30d1d77f1d89538e920fde09d9da4e6a
CarRoc07 <carlovhd@gmail.com>
2023-08-02 13:45:05 -0300

    8

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 import { config } from "dotenv";
 config();
 
-export const ACCESS_KEY = process.env.ACCESS_TOKEN_KEY as string;
+export const ACCESS_KEY = process.env.ACCESS_TOKEN_KEY || "ijdbhwckijb" as string;
 
-export const REFRESH_KEY = process.env.REFRESH_TOKEN_KEY as string;
+export const REFRESH_KEY = process.env.REFRESH_TOKEN_KEY || "dfiwugfiuw" as string;
 
 export const PORT = process.env.PORT;
